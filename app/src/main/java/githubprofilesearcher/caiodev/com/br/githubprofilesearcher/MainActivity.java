18f8ddecbaf47417200d4eb384ee8d89ff2c190f
Caio Costa <caiocvtc0@gmail.com>
2017-09-23 23:49:51 -0300

    First Commit

---
+++ app/src/main/java/githubprofilesearcher/caiodev/com/br/githubprofilesearcher/MainActivity.java
@@ -0,0 +1,146 @@
+package githubprofilesearcher.caiodev.com.br.githubprofilesearcher;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    final private String BASE_URL_ADDRESS = "https://api.github.com/users/";
+    private EditText mSearchProfile;
+    private Button mSearchButton;
+    private CardView mUserCard;
+    private TextView mUserName;
+    private TextView mUserBio;
+    private TextView mUserFollowers;
+    private TextView mUserRepos;
+    private ImageView mUserAvatar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSearchProfile = findViewById(R.id.search_profile);
+
+        mSearchButton = findViewById(R.id.search_button);
+        mSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fillProfileInfo(mSearchProfile.getText().toString());
+            }
+        });
+    }
+
+    void fillProfileInfo(String profile) {
+        String requestUrl = BASE_URL_ADDRESS + profile;
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .get()
+                .url(requestUrl)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.i("getProfileInfo", "FAIL");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String jsonData = response.body().string();
+                Log.i("getProfileInfo", jsonData);
+
+                if (response.isSuccessful()) {
+                    mUserCard = findViewById(R.id.card_user_info);
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            try {
+                                JSONObject rootObj = new JSONObject(jsonData);
+
+                                mUserName = findViewById(R.id.user_name);
+                                mUserBio = findViewById(R.id.user_bio);
+                                mUserFollowers = findViewById(R.id.user_folowers);
+                                mUserRepos = findViewById(R.id.user_repos);
+                                mUserAvatar = findViewById((R.id.user_avatar));
+
+                                mUserName.setText(rootObj.getString(getString(R.string.login_alias)));
+
+                                if(rootObj.getString(getString(R.string.bio_alias)).equals("null")){
+                                    mUserBio.setText(getString(R.string.null_bio_message));
+
+                                } else{
+                                    mUserBio.setText(rootObj.getString(getString(R.string.bio_alias)));
+                                }
+
+                                if(rootObj.getInt(getString(R.string.followers_alias)) <= 1){
+                                    mUserFollowers.setText(getString(R.string.number_of_followers_passing_attributes,
+                                            rootObj.getInt(getString(R.string.followers_alias)),
+                                            getString(R.string.number_of_followers_singular)));
+
+                                } else {
+                                    mUserFollowers.setText(getString(R.string.number_of_followers_passing_attributes,
+                                            rootObj.getInt(getString(R.string.followers_alias)),
+                                            getString(R.string.number_of_followers_plural)));
+                                }
+
+                                if(rootObj.getInt(getString(R.string.public_repos_alias)) <= 1){
+                                    mUserRepos.setText(getString(R.string.number_of_repos_passing_attributes,
+                                            rootObj.getInt(getString(R.string.public_repos_alias)),
+                                            getString(R.string.number_of_repos_singular)));
+
+                                } else {
+                                    mUserRepos.setText(getString(R.string.number_of_repos_passing_attributes,
+                                            rootObj.getInt(getString(R.string.public_repos_alias)),
+                                            getString(R.string.number_of_repos_plural)));
+                                }
+
+                                try {
+                                    Glide.with(MainActivity.this)
+                                            .load(rootObj.getString(getString(R.string.avatar_image_url)))
+                                            .asBitmap()
+                                            .fitCenter()
+                                            .into(mUserAvatar);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                                mUserCard.setVisibility(View.VISIBLE);
+                                fillRepoInfo();
+                            } catch (JSONException e) {
+                                mUserCard.setVisibility(View.GONE);
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    void fillRepoInfo() {
+        // Implemente aqui a sequência
+    }
+}
