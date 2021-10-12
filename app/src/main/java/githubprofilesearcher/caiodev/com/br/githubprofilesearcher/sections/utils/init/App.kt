1276a73007f2364fbbdd8bec14bf490c99383d31
Caio Costa <caiotavaresv39@hotmail.com>
2021-10-12 13:24:37 -0300

    Start using the proper way to load images with coil Update dependencies

--- app/src/main/java/githubprofilesearcher/caiodev/com/br/githubprofilesearcher/sections/utils/init/App.kt
+++ app/src/main/java/githubprofilesearcher/caiodev/com/br/githubprofilesearcher/sections/utils/init/App.kt
@@ -1,20 +1,25 @@
 package githubprofilesearcher.caiodev.com.br.githubprofilesearcher.sections.utils.init
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.util.CoilUtils
 import githubprofilesearcher.caiodev.com.br.githubprofilesearcher.BuildConfig
+import githubprofilesearcher.caiodev.com.br.githubprofilesearcher.R
 import githubprofilesearcher.caiodev.com.br.githubprofilesearcher.sections.profile.model.di.userProfileViewModel
 import githubprofilesearcher.caiodev.com.br.githubprofilesearcher.sections.repository.model.diModules.userRepositoryViewModel
 import githubprofilesearcher.caiodev.com.br.githubprofilesearcher.sections.utils.base.di.globalModule
 import kotlinx.serialization.ExperimentalSerializationApi
+import okhttp3.OkHttpClient
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 import timber.log.Timber
 
-@Suppress("UNUSED")
-class App : Application() {
-    @ExperimentalSerializationApi
+@Suppress("Unused")
+class App : Application(), ImageLoaderFactory {
+    @OptIn(ExperimentalSerializationApi::class)
     override fun onCreate() {
         super.onCreate()
         startKoin {
@@ -23,5 +28,18 @@ class App : Application() {
             modules(globalModule, userProfileViewModel, userRepositoryViewModel)
         }
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(applicationContext)
+            .crossfade(true)
+            .placeholder(R.mipmap.ic_launcher)
+            .okHttpClient {
+                OkHttpClient.Builder()
+                    .cache(CoilUtils.createDefaultCache(applicationContext))
+                    .build()
+            }
+            .error(R.mipmap.ic_launcher)
+            .build()
     }
 }
