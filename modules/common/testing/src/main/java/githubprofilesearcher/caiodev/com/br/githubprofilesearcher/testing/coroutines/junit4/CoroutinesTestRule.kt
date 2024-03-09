1308df8c430e4385f6cd5772e659f2365dec9f9c
Caio Costa <caiotavaresv39@hotmail.com>
2024-03-09 22:43:01 +0000

    Remove deprecated Coroutine Test Code

--- modules/common/testing/src/main/java/githubprofilesearcher/caiodev/com/br/githubprofilesearcher/testing/coroutines/junit4/CoroutinesTestRule.kt
+++ modules/common/testing/src/main/java/githubprofilesearcher/caiodev/com/br/githubprofilesearcher/testing/coroutines/junit4/CoroutinesTestRule.kt
@@ -1,36 +1,5 @@
 package githubprofilesearcher.caiodev.com.br.githubprofilesearcher.testing.coroutines.junit4
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.TestCoroutineScope
-import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.setMain
-import org.junit.rules.TestRule
-import org.junit.runner.Description
-import org.junit.runners.model.Statement
-import kotlin.coroutines.ContinuationInterceptor
+class CoroutinesTestRule {
 
-@ExperimentalCoroutinesApi
-class CoroutinesTestRule : TestRule, TestCoroutineScope by TestCoroutineScope() {
-    val dispatcher = coroutineContext[ContinuationInterceptor] as TestCoroutineDispatcher
-
-    override fun apply(
-        base: Statement,
-        description: Description,
-    ): Statement {
-        return object : Statement() {
-            @Throws(Throwable::class)
-            override fun evaluate() {
-                Dispatchers.setMain(dispatcher)
-
-                // everything above this happens before the test
-                base.evaluate()
-                // everything below this happens after the test
-
-                cleanupTestCoroutines()
-                Dispatchers.resetMain()
-            }
-        }
-    }
 }
